b97a7d4b9e0cbf8bc5c23959c23e499459e0f540
Ben Moseley <bmoseley@armor.com>
2020-02-07 12:28:40 -0600

    revamped

--- windows-firstboot/firstboot.ps1
+++ windows-firstboot/firstboot.ps1
@@ -1,17 +1,4 @@
-param(
-	[parameter]
-	[string]$machType
-)
-
-# confirm machType entry
-if ($machType -eq "") {
-	$machType = Read-Host -prompt "Personal or server?"
-    $machType = $machType.ToLower()
-}
-else {
-    $type = $type.ToLower()
-    Write-Output "Using "$machType.toUpper"script."
-}
+Write-Output "Running Choco check..."
 
 # check and install choco pkg mgr
 $chocoCheck = 'choco version'
@@ -25,29 +12,57 @@ if ($chocoCheck.StartsWith("choco :")) {
         exit 1
     }
 }
-elseif ($chocoCheck.StartsWith("Chocolatey")) {
+else {
     Write-Output "Choco already installed."
 }
-else {
-    Write-Output "Logic mistake during chocolatey check"
+
+$i = "i"
+$counter = 0
+$lib = @{}
+while ($i -ne "0") {
+    $i = Read-Host -prompt "Enter number of each desired application, followed by enter. When done, enter 0. For list, enter 99"
+    if ($i -eq "99") {
+        Write-Output "1) 7zip
+2) steam
+3) steam cmd
+4) discord
+5) chrome
+6) chrome remote desktop
+7) plex server
+8) utorrent
+9) obs
+10) hwinfo
+11) hijackthis
+12) spybot s&d"
+    }
+    elseif ($i -ne "0") {
+        switch ($i) {
+            1 {$n = "7zip"}
+            2 {$n = "steam"}
+            3 {$n = "steamcmd"}
+            4 {$n = "discord"}
+            5 {$n = "googlechrome"}
+            6 {$n = "chrome-remote-desktop-host"}
+            7 {$n = "plex"}
+            8 {$n = "utorrent"}
+            9 {$n = "obs"}
+            10 {$n = "hwinfo"}
+            11 {$n = "hijackthis"}
+            12 {$n = "spybot"}
+        }
+        $lib[$counter] = $n
+        $counter++
+    }
 }
 
-if ($machType = "personal") {
-    $personalType = Read-Host -prompt "Simple or advanced essentials? If you aren't sure, pick simple."
-    $machType = $personalType.ToLower()
+$command = "choco install -y"
+
+foreach ($k in 0..$counter) {
+    $package = $($lib[$k])
+    $command = $command + " $package"
 }
 
-switch ($machType) {
-    "server" {
-        choco install -y 7zip steam steamcmd hwinfo.portable plex treesizefree chrome-remote-desktop-host googlechrome utorrent
-    }
-    "simple" {
-        choco install -y 7zip steam discord googlechrome
-    }
-    "advanced" {
-        choco install -y 7zip steam steamcmd vscode hijackthis hwinfo.portable treesizefree discord obs chrome-remote-desktop-host googlechrome utorrent
-    }
-}
+Invoke-Expression $command
 
 # template for inline .msi installations:
 # Start-Process msiexec.exe -Wait -ArgumentList '/I X:\path\to\msifile /quiet'
