8a6f9d8c747d4af2b96ca7f9381351fe74e3c678
badmoe <botothemose@gmail.com>
2020-11-16 20:08:09 -0600

    idk I did like 5000 things

--- PD2Launch/pd2launch.ps1
+++ PD2Launch/pd2launch.ps1
@@ -13,28 +13,41 @@ if (!$d2path) {
 # Confirmation prompt
 $pathResponse = read-host "Is the path `"$d2path`" correct? (Y/N)"
 switch ($pathResponse) {
-    "y" {write-output "Happy path"; Break}
-    default {write-output "Aborting"}
+    "y" {write-output "Continuing"; break}
+    default {write-output "Aborting"; exit 0}
 }
+# Grab list from bucket
+$bucket = "https://storage.googleapis.com/storage/v1/b/pd2-client-files/o"
+$bucketObj = invoke-webrequest -uri $bucket -method "GET" -usebasicparsing
+$objRaw = convertfrom-json $bucketObj.content
+#$objRange = 0..$($objRaw.items.count-1)
+# Commented out full range, test variable below
+$objRange = 1
 # Scan checksums
-# Options:
-# Iterative - individual checksum, then pull
-# Bulk - run checksums 100%, then pull
+$objRange | foreach-object {
+    # Path $objPath needs delimiters to remove prefix/suffix
+    $objPath = $objRaw.items[$_].id
+    $objGen = $objRaw.items[$_].generation
+    $objHash = $objRaw.items[$_].md5hash
+    $objName = $objRaw.items[$_].name
+    # Path $objPath will need to be inserted below
+    $localName = "${d2path}"+"\ProjectD2\${objName}\$objGen"
+    $localHash = $(get-filehash $localName -algorithm md5).hash
+    if ($objHash -eq $localHash) {
+        write-output "Match! Skipping to next file..."
+        write-output "bucket: $objHash (local: $localHash)"
+        write-output "filename: $objName (local: $localName)"
+    }
+    else {
+        write-output "Unmatched! Downloading updated file..."
+        write-output "bucket: $objHash (local: $localHash)"
+        write-output "filename: $objName (local: $localName)"
+    }
+}
 
 <#
-general layout
-
-#establish diablo path
-##prompt for "is correct"
-#scan checksum files
-##list identified discrepencies
-##continue/abort confirmation
-#grab files
-
-process:
-#foreach $object read $name and $checksum
-##check $d2path+$name for $checksumLocal
-##if $checksum and $checksumLocal match, do nothing
-##elseif $checksum and $checksumLocal do not match:
-###grab $name and place in $d2path+$name
+to do list:
+md5sums not correct locally, possible issue with 'generation' value
+need logic for grabbing the file if matching
+delimiters on the path of the 'id' value to cut out prefix/suffix (pd2 misnomer and generation id)
 #>
